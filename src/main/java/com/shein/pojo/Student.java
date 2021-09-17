bbcc5c81898495382c672fcd886bca74c1229979
yiqizou <acmen_lyf@163.com>
2021-09-17 17:32:20 +0800

    diy

---
+++ src/main/java/com/shein/pojo/Student.java
@@ -0,0 +1,13 @@
+package com.shein.pojo;
+
+import lombok.Data;
+
+/**
+ * @author 80083891
+ * @date 2021/9/17  17:28
+ */
+@Data
+public class Student {
+    private String name;
+    private Integer age;
+}
